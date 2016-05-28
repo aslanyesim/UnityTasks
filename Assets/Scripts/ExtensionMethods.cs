3c9230d8e4ca6727ccc7bb0007eb239d0c9976f6
yesimaslan <aslanyesim90@gmail.com>
2016-05-28 17:11:43 +0200

    Extension Methods 1

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods {
+
+        //Method takes json string converts an object converts it back and return the comparision result in boolean 
+        public static bool jsonDataEqualityTest(string jsonString) {
+            JsonData temporalData = JsonUtility.FromJson<JsonData>(jsonString);                    //convert json string into c# class object
+            string temporalJsonData = JsonUtility.ToJson(temporalData);                            //convert c# class object into json string  
+            return (string.Compare(jsonString, temporalJsonData) == 0);                            //compare json strings and return the result
+    }
+}
--- Assets/Scripts/JsonUnitTest.cs
+++ Assets/Scripts/JsonUnitTest.cs
@@ -6,18 +6,11 @@ public class JsonUnitTest : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        Debug.Log(jsonDataEqualityTest(""));
+       // Debug.Log(jsonDataEqualityTest(""));
         StartCoroutine(jsonGetRequest());
+        ExtensionMethods.jsonDataEqualityTest("");
     }
-
-    //Method takes json string converts an object converts it back and return the comparision result in boolean 
-    public bool jsonDataEqualityTest(string jsonString) {
-        JsonData temporalData = JsonUtility.FromJson<JsonData>(jsonString);                    //convert json string into c# class object
-        string temporalJsonData = JsonUtility.ToJson(temporalData);                            //convert c# class object into json string  
-        return (string.Compare(jsonString, temporalJsonData) == 0);                            //compare json strings and return the result
-    }
-
-
+   
     IEnumerator jsonGetRequest() {
         UnityWebRequest www = UnityWebRequest.Get("http://httpbin.org/get");                    //get json data
         yield return www.Send();
