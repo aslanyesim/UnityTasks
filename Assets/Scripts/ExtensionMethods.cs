b8245136e37d55b6823cf94dc1c40bb2d75b7f6c
yesimaslan <aslanyesim90@gmail.com>
2016-05-28 22:48:20 +0200

    Extension Methods 4

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -2,11 +2,11 @@
 
 public static class ExtensionMethods {
 
-        //Method takes json string converts an object converts it back and return the comparision result in boolean 
-        public static bool jsonDataEqualityTest(string jsonString) {
-            JsonData temporalData = JsonUtility.FromJson<JsonData>(jsonString);                    //convert json string into c# class object
-            string temporalJsonData = JsonUtility.ToJson(temporalData);                            //convert c# class object into json string  
-            return (string.Compare(jsonString, temporalJsonData) == 0);                            //compare json strings and return the result
+    //Method takes json string converts an object converts it back and return the comparision result in boolean 
+    public static bool jsonDataEqualityTest(string jsonString) {
+        JsonData temporalData = JsonUtility.FromJson<JsonData>(jsonString);                    //convert json string into c# class object
+        string temporalJsonData = JsonUtility.ToJson(temporalData);                            //convert c# class object into json string  
+        return (string.Compare(jsonString, temporalJsonData) == 0);                            //compare json strings and return the result
     }
 
     /// <summary>
@@ -19,7 +19,7 @@ public static class ExtensionMethods {
             obj.AddComponent(typeof(ComponentA));                                                   //add the new component
             Debug.Log("New " + typeof(ComponentA) + " component added");
         } else {
-             obj.GetComponent<ComponentA>();                                                        //return the existing component
+            obj.GetComponent<ComponentA>();                                                        //return the existing component
         }
     }
 }
--- Assets/Scripts/JsonUnitTest.cs
+++ Assets/Scripts/JsonUnitTest.cs
@@ -10,14 +10,13 @@ public class JsonUnitTest : MonoBehaviour {
         ExtensionMethods.jsonDataEqualityTest("");
         gameObject.getOrAddComponent<Rigidbody>();
     }
-   
     IEnumerator jsonGetRequest() {
         UnityWebRequest www = UnityWebRequest.Get("http://httpbin.org/get");                    //get json data
         yield return www.Send();
         if (www.isError) {
             Debug.Log(www.error);
         } else {
-            JsonData temporalData = JsonUtility.FromJson<JsonData>(www.downloadHandler.text);              
+            JsonData temporalData = JsonUtility.FromJson<JsonData>(www.downloadHandler.text);
             Debug.Log(temporalData.headers.Cookie);                                             //print the result
         }
     }
