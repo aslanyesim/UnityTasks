b9fe17b15d172993e33d510235d6859934b370f8
yesimaslan <aslanyesim90@gmail.com>
2016-05-28 14:56:22 +0200

    Unity Tests 1

---
+++ Assets/Scripts/JsonData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+[Serializable]
+public class JsonData {
+    //Json data variables 
+    public Args args;
+    public Headers headers;
+    public string origin;
+    public string url;
+
+    public class Args {
+    }
+
+    public class Headers {
+        public string accept;
+        public string acceptEncoding;
+        public string acceptLanguage;
+        public string host;
+        public string upgradeInsecureRequests;
+        public string userAgent;
+    }
+}
---
+++ Assets/Scripts/JsonUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class JsonUnitTest : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log(jsonDataEqualityTest(""));
+    }
+
+    //Method takes json string converts an object converts it back and return the comparision result in boolean 
+    public bool jsonDataEqualityTest(string jsonString) {
+        JsonData temporalData = JsonUtility.FromJson<JsonData>(jsonString);                    //convert json string into c# class object
+        string temporalJsonData = JsonUtility.ToJson(temporalData);                            //convert c# class object into json string  
+        return (string.Compare(jsonString, temporalJsonData) == 0);                            //compare json strings and return the result
+    }
+}
