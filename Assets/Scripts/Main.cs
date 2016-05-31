a378167248868d8c26b8c2ba7aaf3194d80de8ba
yesimaslan <aslanyesim90@gmail.com>
2016-05-31 17:45:08 +0200

    A generic onClick Monobehaviour 2

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,12 +5,12 @@ public class Main : MonoBehaviour, IPointerClickHandler {
     public GameObject obj;
 
     public delegate void OnPointerClicked();
-    public static event OnPointerClicked OnClicked;
+    public event OnPointerClicked OnClicked;
 
     // Use this for initialization
     void Start() {
         OnClicked = delegate () {
-            OnClicked += obj.GetComponent<OnClickMono>().OnPointerClicked;                      //delegates the OnClickMono 
+            OnClicked += obj.GetComponent<OnClickMono>().OnPointerClicked;                             //delegates the OnClickMono 
             OnClicked += obj.GetComponent<OnClickParentInfo>().OnPointerClicked;                       //delegates the ParentInfo 
             OnClicked += obj.GetComponent<OnClickHide>().OnPointerClicked;                             //delegates the Hide 
         };
@@ -19,4 +19,4 @@ public class Main : MonoBehaviour, IPointerClickHandler {
     public void OnPointerClick(PointerEventData eventData) {
         OnClicked();                                                                            //executes onclicked when the target has been clicked
     }
-}
+    }
